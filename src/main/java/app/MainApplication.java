da822aa9c1e643c3858694ae07f4ab379c99d3c6
Tatu <tatu.sailynoja@gmail.com>
2024-09-06 15:13:34 +0300

    controller fix

--- src/main/java/app/MainApplication.java
+++ src/main/java/app/MainApplication.java
@@ -3,7 +3,7 @@ package app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = { "controller" })
 public class MainApplication {
     public static void main(String[] args) {
         // Main loop, listens for web requests
