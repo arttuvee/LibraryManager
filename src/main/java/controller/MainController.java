9c20cb75f59f47a255fcd478131381d2c27fc802
Tatu <tatu.sailynoja@gmail.com>
2024-09-06 16:01:00 +0300

    getalluser toimi?

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -1,15 +1,32 @@
 package controller;
 
+import model.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class MainController {
 
+    private final UserController userController;
+
+    @Autowired
+    public MainController(UserController userController) {
+        this.userController = userController;
+    }
+
     @GetMapping("/hello")
     public String hello() {
         return "Hello, World!";
     }
+
+    @GetMapping("/users")
+    public List<User> getAllUsers() {
+        return userController.getAllUsers();
+    }
+
 }
--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -2,16 +2,26 @@ package controller;
 
 import database.UserDAO;
 import model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.sql.SQLException;
 import java.util.List;
 
+@RestController
+@RequestMapping("/users")
 public class UserController {
     private UserDAO userDAO;
 
+    @Autowired
     public UserController() {
         this.userDAO = new UserDAO();
     }
 
+    @GetMapping
     public List<User> getAllUsers() {
         try {
             return userDAO.getAllUsers();
@@ -21,6 +31,7 @@ public class UserController {
         }
     }
 
+    @GetMapping("/{id}")
     public User getUserById(int id) {
         try {
             return userDAO.getUserById(id);
