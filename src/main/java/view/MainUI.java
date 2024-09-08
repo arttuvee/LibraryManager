d8d836920edd666f569b77ab3560c4359391ad41
arttuvee <arttuvo@metropolia.fi>
2024-09-08 14:48:28 +0300

    Alottalin käyttöliittymää

---
+++ src/main/java/view/MainUI.java
@@ -0,0 +1,13 @@
+package view;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainUI {
+
+}
