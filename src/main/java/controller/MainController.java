f77728dc68b8c7d02bfcc745c7d937c8bae7617e
tatus <tatu.sailynoja@gmail.com>
2024-09-09 13:10:54 +0300

    lokaaliin testaukseen

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -1,11 +1,9 @@
 package controller;
 
-import model.User;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -16,9 +14,6 @@ public class MainController {
         return "Hello, World!";
     }
 
-    @GetMapping("/users")
-    public List<User> getAllUsers() {
-        return UserController.getAllUsers();
-    }
+
 
 }
--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,12 +1,18 @@
 package controller;
 
-import database.UserDAO;
-import model.User;
-import org.springframework.web.bind.annotation.GetMapping;
-
 import java.sql.SQLException;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import database.UserDAO;
+import model.User;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
 
     @GetMapping
@@ -20,7 +26,7 @@ public class UserController {
     }
 
     @GetMapping("/{id}")
-    public static User getUserById(int id) {
+    public static User getUserById(@PathVariable int id) {
         try {
             return UserDAO.getUserById(id);
         } catch (SQLException e) {
