35e68eb2ef9a39f994962c384bfdc10343526c63
arttuvee <arttuvo@metropolia.fi>
2024-09-09 13:46:17 +0300

    LoginController, nappejen fxid:t ja toimintoja

--- src/main/java/view/MainUI.java
+++ src/main/java/view/MainUI.java
@@ -1,13 +1,24 @@
 package view;
 
-import javafx.fxml.FXML;
+import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
+import java.util.Objects;
 
-public class MainUI {
+public class MainUI extends Application {
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/LoginView.fxml")));
+        primaryStage.setTitle("Login");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);  // Launch JavaFX Application
+    }
 }
